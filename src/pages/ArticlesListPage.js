6eb2e372008a42c47f2b368db27861e8fd83046a
Mendel Kahan <mendy@famkahan.com>
2021-02-21 22:39:21 +0000

    h

---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import articleContent from './article-content';
+import ArticleList from "../components/ArticlesList";
+
+
+ const ArticlesListPage = () => (
+     <>
+         <h1>Articles</h1>
+         <ArticleList articles={articleContent}/>
+
+   
+
+
+     </>
+ );
+
+
+ export default ArticlesListPage;
---
+++ src/pages/notFoundPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const NotFoundPage = () => <h1>Page Not found</h1>;
+
+export default NotFoundPage;
